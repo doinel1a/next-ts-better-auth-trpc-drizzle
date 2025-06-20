eb4efab44d08a7a696853208d3b5047e5c79ec29
doinel1a <doinel1atanasiu@gmail.com>
2025-06-20 22:07:57 +0200

    chore: create basic users schema

---
+++ drizzle/schemas/users.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const usersSchema = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp('created_at', { mode: 'date', precision: 3 }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'date', precision: 3 }).$onUpdateFn(() => new Date())
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import db from 'drizzle';
+import { usersSchema } from 'drizzle/schemas/users';
 import dynamic from 'next/dynamic';
 
 import { Skeleton } from '@/components/ui/skeleton';
@@ -8,7 +10,10 @@ const Counter = dynamic(() => import('@/components/counter'), {
   loading: () => <Skeleton className='h-44 w-72 rounded-md' />
 });
 
-export default function HomePage() {
+export default async function HomePage() {
+  const users = await db.select().from(usersSchema);
+  console.log('users', users);
+
   return (
     <main className='flex h-full w-full flex-col items-center justify-center'>
       <Counter />

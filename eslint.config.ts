87ccec4172816e467e234eabeb8c1311f5150045
doinel1a <doinel1atanasiu@gmail.com>
2026-03-29 12:33:22 +0200

    chore: update eslint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -15,56 +15,14 @@ export default defineConfig(
   },
   /**
    * eslint-config-next automatically configures:
-   * - eslint-plugin-next
    * - eslint-plugin-react
    * - eslint-plugin-react-hooks
    * - eslint-plugin-jsx-a11y
    */
   ...eslintConfigNext,
   ...tanstackQuery.configs['flat/recommended'],
-  {
-    plugins: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      drizzle: drizzlePlugin
-    },
-    rules: {
-      'drizzle/enforce-delete-with-where': ['error', { drizzleObjectName: 'db' }],
-      'drizzle/enforce-update-with-where': ['error', { drizzleObjectName: 'db' }]
-    }
-  },
-  prettierConfig,
-  prettierPlugin,
   sonarjs.recommended,
   unicorn.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    extends: [
-      ...tseslint.configs.strict,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylistic,
-      ...tseslint.configs.stylisticTypeChecked
-    ],
-    rules: {
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/consistent-type-definitions': 'off',
-      '@typescript-eslint/consistent-type-imports': [
-        'warn',
-        { prefer: 'type-imports', fixStyle: 'inline-type-imports' }
-      ],
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/require-await': 'off',
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: { attributes: false } }
-      ],
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        {
-          allowNumber: true
-        }
-      ]
-    }
-  },
   {
     linterOptions: {
       reportUnusedDisableDirectives: true
@@ -76,18 +34,113 @@ export default defineConfig(
     }
   },
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
     rules: {
-      'unicorn/no-null': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' }
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'typeLike', format: ['PascalCase'] },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow'
+        },
+        { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow' }
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } }
+      ],
+      '@typescript-eslint/no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'react',
+              importNames: ['default'],
+              message:
+                "Only named imports are allowed from 'react' (e.g. import { useState } from 'react')."
+            }
+          ]
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowNumber: true,
+          allowBoolean: true
+        }
+      ],
+      '@typescript-eslint/strict-boolean-expressions': [
+        'error',
+        {
+          allowNullableBoolean: true,
+          allowNullableNumber: true
+        }
+      ],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off'
+    }
+  },
+  prettierConfig,
+  prettierPlugin,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      drizzle: drizzlePlugin
+    },
+    rules: {
+      'drizzle/enforce-update-with-where': ['error', { drizzleObjectName: 'db' }],
+      'drizzle/enforce-delete-with-where': ['error', { drizzleObjectName: 'db' }]
+    }
+  },
+  {
+    rules: {
+      curly: ['error', 'all'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+      'react/hook-use-state': 'error',
+      'react/jsx-no-useless-fragment': 'error',
+      'react/no-array-index-key': 'error',
+      'react/no-unstable-nested-components': 'error',
+      'react/self-closing-comp': 'error',
+
       'unicorn/prevent-abbreviations': [
         'error',
         {
           allowList: {
             env: true,
-            ctx: true
-          },
-          ignore: [/db/i, /params/i]
+            db: true,
+            ctx: true,
+            params: true,
+            searchParamsKey: true,
+            globalForDb: true
+          }
         }
-      ]
+      ],
+
+      'sonarjs/deprecation': 'off', // Covered by @typescript-eslint
+      'sonarjs/no-array-delete': 'off', // Covered by @typescript-eslint
+      'sonarjs/no-unused-vars': 'off', // Covered by @typescript-eslint
+      'sonarjs/prefer-regexp-exec': 'off', // Covered by @typescript-eslint
+      'sonarjs/unused-import': 'off', // Covered by @typescript-eslint
+
+      'unicorn/no-array-sort': 'off',
+      'unicorn/no-nested-ternary': 'off', // Covered by sonarjs/no-nested-conditional
+      'unicorn/no-null': 'off',
+      'unicorn/no-useless-undefined': 'off',
+      'unicorn/prefer-includes': 'off', // Covered by @typescript-eslint
+      'unicorn/prefer-string-starts-ends-with': 'off' // Covered by @typescript-eslint
     }
   }
 );

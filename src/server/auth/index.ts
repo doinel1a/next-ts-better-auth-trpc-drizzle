1a4633a2f2b3fe68fb6a3afaf3eeac6156a999aa
doinel1a <doinel1atanasiu@gmail.com>
2026-05-08 20:23:48 +0200

    fix(server/auth): use path alias in imports

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -2,8 +2,8 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { nextCookies } from 'better-auth/next-js';
 
-import db from '../../../drizzle';
-import * as schema from '../../../drizzle/schemas/auth';
+import db from '~/drizzle';
+import * as schema from '~/drizzle/schemas/auth';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {

7aa050c7d13ac54db5086b40c17c8a7e4720f701
doinel1a <doinel1atanasiu@gmail.com>
2026-05-08 10:58:27 +0200

    fix(server/auth/server): handle get-session error

--- src/server/auth/server.ts
+++ src/server/auth/server.ts
@@ -1,7 +1,18 @@
+import 'server-only';
+
 import { cache } from 'react';
 
 import { headers } from 'next/headers';
 
+import { serverLogger } from '@/lib/utils/logger/server';
+
 import { auth } from '.';
 
-export const getSession = cache(async () => auth.api.getSession({ headers: await headers() }));
+export const getSession = cache(async () => {
+  try {
+    return await auth.api.getSession({ headers: await headers() });
+  } catch (error) {
+    serverLogger.error('Cannot get session', error);
+    return null;
+  }
+});
